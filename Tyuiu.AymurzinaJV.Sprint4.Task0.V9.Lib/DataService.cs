667a98251400474df910d272f22ab8023abaefc0
AymurzinaJulia <aymurzinajulia@gmail.com>
2025-11-27 02:33:05 +0500

    Add project files.

---
+++ Tyuiu.AymurzinaJV.Sprint4.Task0.V9.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.AymurzinaJV.Sprint4.Task0.V9.Lib
+{
+    public class DataService : ISprint4Task0V9
+    {
+        public int GetSumEvenArrEl(int[] array)
+        {
+            int i;
+            int sum = 0;
+            for (i = 0; i <= 9; i += 2)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.AymurzinaJV.Sprint4.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AymurzinaJV.Sprint4.Task0.V9.Lib;
+namespace Tyuiu.AymurzinaJV.Sprint4.Task0.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 4, 6, 2, 8, 4, 5, 6, 9, 8, 7 };
+            int wait = 24;
+            int res = ds.GetSumEvenArrEl(array);
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
