64f560221fada81e02bb47e6984f21e88781141e
AymurzinaJulia <aymurzinajulia@gmail.com>
2025-11-28 01:40:16 +0500

    Task4 added

---
+++ Tyuiu.AymurzinaJV.Sprint4.Task4.V8.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.AymurzinaJV.Sprint4.Task4.V8.Lib;
+namespace Tyuiu.AymurzinaJV.Sprint4.Task4.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] array = {{ 3, 6, 1, 5, 3 },
+                            { 2, 3, 5, 1, 1 },
+                            { 1, 2, 5, 5, 1 },
+                            { 3, 6, 5, 6, 2 },
+                            { 3, 6, 4, 5, 5 }};
+
+            int[,] wait =  {{ 3, 1, 1, 5, 3 },
+                            { 1, 3, 5, 1, 1 },
+                            { 1, 1, 5, 5, 1 },
+                            { 3, 1, 5, 1, 1 },
+                            { 3, 1, 1, 5, 5 }};
+            int[,] res = ds.Calculate(array);
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AymurzinaJV.Sprint4.Task4.V8/Program.cs
@@ -0,0 +1,65 @@
+﻿using Tyuiu.AymurzinaJV.Sprint4.Task4.V8.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #4 | Выполнила: Аймурзина Ю. В. | РППб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                            *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #8                                                              *");
+Console.WriteLine("* Выполнила: Аймурзина Юлия Владимировна | РППб-25-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+Console.WriteLine("* значениями с клавиатуры в диапазоне от 1 до 6.                          *");
+Console.WriteLine("* Заменить четные элементы массива на 1.                                  *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int rows;
+Console.WriteLine("Введите количество строк в массиве: ");
+rows = Convert.ToInt32(Console.ReadLine());
+
+int columns;
+Console.WriteLine("Введите количество столбцов в массиве: ");
+columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] numsArray = new int[rows, columns];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"Введите {i},{j} элемент массива: ");
+        numsArray[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+    
+}
+
+Console.WriteLine();
+Console.WriteLine("Массив: ");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(numsArray[i,j] + "\t");
+    }
+
+}
+
+Console.WriteLine();
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int[,] res = ds.Calculate(numsArray);
+
+foreach (int i in res)
+    Console.Write(i + "\t");
+
+Console.ReadKey();
